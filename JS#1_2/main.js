10d652fefccc650ec49dbe585c180921a1551af8
Andrii Hovorkov <mrlandersonl97@gmail.com>
2020-11-14 21:44:34 +0200

    JS#1_2

---
+++ JS#1_2/main.js
@@ -0,0 +1,39 @@
+var a = prompt('a'),
+    b = prompt('b'),
+    c = prompt('c')   
+;
+aNanber = parseInt(a);
+bNanber = parseInt(b);
+cNanber = parseInt(c);
+
+var abc = ( aNanber + bNanber + cNanber);
+alert(abc);
+
+aN = aNanber % 2 ;
+bN = bNanber % 2 ;
+cN = cNanber % 2 ;
+
+var aEven = '?';
+var bEven = '?';
+var cEven = '?';
+
+if (aN == 1,2,3,4,5,6,7,8,9) {
+    var aEven = 'не чётное';
+}
+if(aN == 0) {
+    var aEven= 'чётное';
+}
+
+if (bN == 1,2,3,4,5,6,7,8,9) {
+    var bEven = 'не чётное';
+}
+if(bN == 0) {
+    var bEven = 'чётное';
+}
+
+if (cN == 1,2,3,4,5,6,7,8,9) {
+    var cEven= 'не чётное';
+}
+if(cN == 0) {
+    var cEven = 'чётное';
+}
