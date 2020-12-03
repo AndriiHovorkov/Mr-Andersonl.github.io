0e86622a72a70c0a7c214bf5895f07ac8453ebf1
Andrii Hovorkov <mrlandersonl97@gmail.com>
2020-12-03 22:37:45 +0200

    JS#6_2

--- JS#6_2/main.js
+++ JS#6_2/main.js
@@ -1,11 +1,5 @@
 A =[23,34,46,'movie',45,'Game'];
 B =[3,4,6,'movie',5,'Game'];
-console.log(A);
-console.log(B);
-
-
-line ='__________________________________________'
-console.log(line);
 
 function sumArr(z){
     var sum = 0;
@@ -13,7 +7,6 @@ function sumArr(z){
         if (typeof z[i] == "number"){
             sum += z[i];
         }
-        
     }
     return sum;
 }
@@ -22,12 +15,8 @@ sumArr(B)
 
 function compare(a, b){
     if(sumArr(a) > sumArr(b)){
-        console.log(A); 
-        console.log(sumArr(a));
         return A;
     }else {
-        console.log(B); 
-        console.log(sumArr(b));
         return B;
     }
 }
