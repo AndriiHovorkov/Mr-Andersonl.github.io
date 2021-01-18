7beffe8e68d2cc2d1ca5e69b51dcf0fb069c907f
Andrii Hovorkov <mrlandersonl97@gmail.com>
2021-01-18 21:21:49 +0200

    JS#16_1

--- Js/JS#16_1/main.js
+++ Js/JS#16_1/main.js
@@ -1,28 +1,27 @@
 window.addEventListener('load', function(){
 
     class C1{
-        constructor() {
-            this.property1 = 'p2';
+        constructor(p2) {
+            this.p2 = p2;
         }
     }
     
-    
     class C2 extends C1 {
-        constructor() {
-            super();
-            this.property2 = 'p1';
-            this.property3 = 'p3';
+        constructor(p2, p1, p3) {
+            super(p2);
+            this.p1 = p1;
+            this.p3 = p3;
         }
     }
     
     class C3 extends C2 {
-        constructor() {
-            super();
-            this.property4 = 'p4';
+        constructor(p2, p1, p3, p4) {
+            super(p2, p1, p3);
+            this.p4 = p4;
         } 
     }
     
-    const chainlet = new C3();
-    console.log(chainlet);
+    const chainlet = new C3(1, 2, 3, 4);
 
+    console.log(chainlet);
 })
