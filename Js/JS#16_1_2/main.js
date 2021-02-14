28b4e6cd836009035ca7e6e5a3864d453bb37888
Andrii Hovorkov <mrlandersonl97@gmail.com>
2021-02-14 16:48:48 +0200

    JS#16_1_2

--- Js/JS#16_1_2/main.js
+++ Js/JS#16_1_2/main.js
@@ -9,140 +9,159 @@ window.addEventListener('load', function(){
             * Курс применяется только к total
     */
 
-const products = [
-    {
-        id: "id-1",
-        name: 'Title 1',
-        price: '100',
-        priceEur: '2.99',
-        priceUsd: '3.60',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg',
-    },
-    {
-        id: "id-2",
-        name: 'Title 2',
-        price: '300',
-        priceEur: '8.97',
-        priceUsd: '10.81',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg',
-    }
-];
+    const products = [
+        {
+            id: "id-1",
+            name: 'Title-1',
+            price: '100',
+            priceEur: '2.99',
+            priceUsd: '3.60',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg',
+        },
+        {
+            id: "id-2",
+            name: 'Title-2',
+            price: '300',
+            priceEur: '8.97',
+            priceUsd: '10.81',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg',
+        }
+    ];
 
-let totalCost = 0;
-const productsContainer =  document.querySelector('.products');
+    let totalCost = 0;
+    const productsContainer =  document.querySelector('.products');
 
-productsContainer.innerHTML = `${products.map(item => `
-    <div class="product solid">   
-        <div class="product__title">${item.name}</div>
-        <img class="product__image" src="${item.image}">
-        <div class="product__price">
-            <button class="product__action" data-product-id="${item.id}">Add</button>
-            <span>${item.price}</span>
-        </div>  
-    </div>
-`).join('')}`;
+    productsContainer.innerHTML = `${products.map(item => `
+        <div class="product solid">   
+            <div class="product__title">${item.name}</div>
+            <img class="product__image" src="${item.image}">
+            <div class="product__price">
+                <button class="product__action" data-product-id="${item.id}">Add</button>
+                <span>${item.price}</span>
+            </div>  
+        </div>
+    `).join('')}`;
 
-const buttons = productsContainer.querySelectorAll('button.product__action');
-const totalContainer = document.querySelector('#total')
+    const minPlu =  document.querySelector('.min-plu');
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', onButtonAddClick)
-})
+    minPlu.innerHTML = `${products.map(item => `
+        <div class="cont">   
+            <button class="sing minus" data-product-id="${item.id}">-</button>
+            <span>
+                ${item.name}:
+                <span id="${item.name}">0</span>
+            </span>
+            <button class="sing plus" data-product-id="${item.id}">+</button>
+        </div>
+            
+    `).join('')}`;
 
-/* const cartAmoun = document.querySelector('.cart_amoun');
-const buttonsMinPlu = cartAmoun.querySelectorAll('button.sing');
+    const total1 = document.querySelector('#Title-1');
+    const total2 = document.querySelector('#Title-2');
 
-buttonsMinPlu.forEach(btn => {
-    btn.addEventListener('click',  onButtonAddClick)
-}) */
+    const buttons = productsContainer.querySelectorAll('button.product__action');
+    const totalContainer = document.querySelector('#total');
 
+    const minButtons = minPlu.querySelectorAll('button.minus');
+    const plusButtons = minPlu.querySelectorAll('button.plus');
 
-let cost1 = 0;
-let cost2 = 0;
-const totalContainer1 = document.querySelector('#total-1')
-const totalContainer2 = document.querySelector('#total-2')
+    buttons.forEach(btn => {
+        btn.addEventListener('click', onButtonAddClick)
+    })
 
-const clearAll =  document.querySelector('.clear');
-const minusTotal1 = document.querySelector('.minus-total1')
-const minusTotal2 = document.querySelector('.minus-total2')
-const plusTotal1 = document.querySelector('.plus-total1')
-const plusTotal2 = document.querySelector('.plus-total2')
+    plusButtons.forEach(btn => {
+        btn.addEventListener('click', onButtonAddClick)
+    })
 
-function onButtonAddClick(event) {
-    const productId = event.target.dataset.productId;
-    const currentProduct = products.find(item => item.id == productId);
+    minButtons.forEach(btn => {
+        btn.addEventListener('click', minClick)
+    })
 
-    currentProduct.id === "id-1"? (cost1 += 1, totalContainer1.innerHTML = cost1):
-        (cost2 += 1, totalContainer2.innerHTML = cost2);
-
+    let cost1 = 0;
+    let cost2 = 0;
+    const clearAll =  document.querySelector('.clear');
     const selind = document.getElementById("select");
 
-    if(selind.value == 'value1'){
-        PlusMin(100, 300, currentProduct.price);
+    function Clear(){
+        clearAll.addEventListener('click', function () {
+            totalContainer.innerHTML = 0;
+            total1.innerHTML = 0;
+            total2.innerHTML = 0;
+            cost1 = 0;
+            cost2 = 0;
+            totalCost = 0;
+        })
     }
-    if(selind.value == 'value2'){
-        PlusMin(2.99, 8.97, currentProduct.priceEur);
+
+    function selindChange() {
+        selind.addEventListener('change', function () {
+            totalContainer.innerHTML = 0;
+            total1.innerHTML = 0;
+            total2.innerHTML = 0;
+            cost1 = 0;
+            cost2 = 0;
+            totalCost = 0;
+        }) 
     }
-    if(selind.value == 'value3'){
-        PlusMin(3.60, 10.81, currentProduct.priceUsd);
-    }
-    
-    selind.addEventListener('change', function () {
-        totalContainer.innerHTML = 0;
-        totalContainer1.innerHTML = 0;
-        totalContainer2.innerHTML = 0;
-        cost1 = 0;
-        cost2 = 0;
-        totalCost = 0;
-        
-    })
 
-    clearAll.addEventListener('click', function () {
-        totalContainer.innerHTML = 0;
-        totalContainer1.innerHTML = 0;
-        totalContainer2.innerHTML = 0;
-        cost1 = 0;
-        cost2 = 0;
-        totalCost = 0;
-    })
+    function onButtonAddClick(event, ) {
+        const productId = event.target.dataset.productId;
+        const currentProduct = products.find(item => item.id == productId);
 
-}
-
-function PlusMin(num1, num2, pri){
-    totalCost += Number(pri);
-    totalContainer.innerHTML = Math.round(totalCost);
-    
-    minusTotal1.addEventListener('click', function () {
-        if(cost1 >= 1){
-            totalCost -= num1;
-            totalContainer.innerHTML = Math.round(totalCost);
-            cost1 -= 1;
-            totalContainer1.innerHTML = cost1;
+        if(selind.value == 'value1'){
+            totalCost += +currentProduct.price;
         }
-    })
-
-    plusTotal1.addEventListener('click', function () {
-        totalCost += num1;
-        totalContainer.innerHTML = Math.round(totalCost);
-        cost1 += 1;
-        totalContainer1.innerHTML = cost1;
-    })
-
-    minusTotal2.addEventListener('click', function () {
-        if(cost2 >= 1){
-            totalCost -= num2;
-            totalContainer.innerHTML = Math.round(totalCost);
-            cost2 -= 1;
-            totalContainer2.innerHTML = cost2;
+        if(selind.value == 'value2'){
+            totalCost += +currentProduct.priceEur;
         }
-    })
-
-    plusTotal2.addEventListener('click', function () {
-        totalCost += num2;
+        if(selind.value == 'value3'){
+            totalCost += +currentProduct.priceUsd;
+        }
+        selindChange()
         totalContainer.innerHTML = Math.round(totalCost);
-        cost2 += 1;
-        totalContainer2.innerHTML = cost2;
-    } )
-} 
+
+        currentProduct.id === "id-1"? (cost1 += 1, total1.innerHTML = cost1):
+            (cost2 += 1, total2.innerHTML = cost2);
+
+        Clear()
+    }
+
+    function minClick(event) {
+        if(totalCost >= 1){
+            const productId = event.target.dataset.productId;
+            const currentProduct = products.find(item => item.id == productId);
+
+            function ifMinus(){
+                if(selind.value == 'value1'){
+                    totalCost -= currentProduct.price;
+                }
+                if(selind.value == 'value2'){
+                    totalCost -= currentProduct.priceEur;
+                }
+                if(selind.value == 'value3'){
+                    totalCost -= currentProduct.priceUsd;
+                }
+                totalContainer.innerHTML = Math.round(totalCost);
+            }
+
+            selindChange()
+
+            if(currentProduct.id =="id-1"){
+                if(cost1 >= 1){
+                    cost1 -= 1; 
+                    total1.innerHTML = cost1;
+                    ifMinus();
+                }
+            }
+            if(currentProduct.id =="id-2"){
+                if(cost2 >= 1){
+                    cost2 -= 1; 
+                    total2.innerHTML = cost2;
+                    ifMinus();
+                }
+            }
+        }
+        Clear()
+    }
 
 })
